089fa52bf22dd4f39a924b539b61e32609802107
Kristýna Fuchsová <kristynafuchsova@seznam.cz>
2025-03-22 17:55:36 +0100

    feat: clickable games and readable time played

--- src/api/vanity-resolver.tsx
+++ src/api/vanity-resolver.tsx
@@ -1,6 +1,6 @@
 "use server"
 
-interface SteamAPIResponse {
+interface SteamApiIdResponse {
     response: {
         steamid?: string;
         success: number;
@@ -10,11 +10,11 @@ interface SteamAPIResponse {
 
 export const getUserIdFromVanity = async (vanity: string): Promise<{ ok: boolean, id?: string, message?: string }> => {
     const key = process.env.STEAM_KEY;
-
     const url = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${key}&vanityurl=${vanity}`;
+
     try {
         const response = await fetch(url);
-        const data: SteamAPIResponse = await response.json();
+        const data: SteamApiIdResponse = await response.json();
 
         if (data.response.success === 1) {
             return {ok: true, id: data.response.steamid};
--- src/components/game-table/game-table.tsx
+++ src/components/game-table/game-table.tsx
@@ -2,6 +2,15 @@
 import "./game-table.css";
 import {Games} from "@/api/all-games-fetcher";
 
+const minutesToReadable = (minutes: number): string => {
+    if (minutes == 0) {
+        return "";
+    }
+    const hours = Math.floor(minutes / 60);
+    const remainedMinutes = minutes % 60;
+    return `${hours} ${hours == 1 ? "hour" : "hours"} ${remainedMinutes} ${remainedMinutes == 1 ? "min" : "mins"}`;
+};
+
 export default function GameTable({data}: { data: Games[] }) {
     return (
         <table className="game-table">
@@ -17,9 +26,9 @@ export default function GameTable({data}: { data: Games[] }) {
             {data.map((game, index) => (
                 <tr className="body-tr" key={index}>
                     <td><span className="boring">{game.appid}</span></td>
-                    <td><span className="interesting">{game.name}</span></td>
-                    <td>{game.playtime_2weeks ? <span className="boring">{game.playtime_2weeks}</span> : <span className="boring empty">0</span>}</td>
-                    <td><span className="boring">{game.playtime_forever}</span></td>
+                    <td><a href={`https://store.steampowered.com/app/${game.appid}`} target="_blank" className="interesting">{game.name}</a></td>
+                    <td>{game.playtime_2weeks ? <span className="boring">{minutesToReadable(game.playtime_2weeks)}</span> : <span className="boring empty">0</span>}</td>
+                    <td>{game.playtime_forever ? <span className="boring">{minutesToReadable(game.playtime_forever)}</span> : <span className="boring empty">0</span>}</td>
 
                 </tr>
             ))}
